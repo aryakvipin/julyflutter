f594d058c16ae3b426f0c190db65c04bc2dc86f8
aryakvipin <arya28985@gmail.com>
2024-12-03 13:11:40 +0530

    commit

---
+++ lib/mediaquery/layoutbuildr/oriantion bulder.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(home:Oriantion() ,));
+}
+class Oriantion extends StatefulWidget {
+  const Oriantion({super.key});
+
+  @override
+  State<Oriantion> createState() => _OriantionState();
+}
+
+class _OriantionState extends State<Oriantion> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: OrientationBuilder(builder: (context,orientation){
+        if(orientation==Orientation.portrait){
+          return Container(
+            color: Colors.yellow,
+            child: Center(
+              child: Text("portrait"),
+            ),
+          );
+        }
+        else{
+          return Container(
+            color: Colors.green,
+            child: Center(
+              child: Text("landscape"),
+            ),
+          );
+        }
+      }),
+    );
+  }
+}
